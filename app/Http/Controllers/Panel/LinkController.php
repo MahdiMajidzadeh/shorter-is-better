75990585d6324a5ec6ce9b3c3b6724aed5a8b73a
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-04-26 13:36:53 +0430

    controllers

---
+++ app/Http/Controllers/Panel/LinkController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LinkController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/RedirectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RedirectController extends Controller
+{
+    public function root()
+    {
+
+    }
+
+    public function  link($slug)
+    {
+
+    }
+}
