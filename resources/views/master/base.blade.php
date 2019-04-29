da368a99815e71b4fc9a5cb4533ceb2c78edad66
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2019-04-29 08:28:42 +0430

    logo

--- resources/views/master/base.blade.php
+++ resources/views/master/base.blade.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <link href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link type="text/css" href="{{ asset('css/argon.css?v=1.0.0') }}" rel="stylesheet">
+    <link rel="icon" href="{{ asset('img/favicon.png') }}" type="image/png">
 </head>
 
 <body>
--- resources/views/master/sidebar.blade.php
+++ resources/views/master/sidebar.blade.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
     <div class="container-fluid">
-        <!-- Toggler -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidenav-collapse-main" aria-controls="sidenav-main" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <!-- Brand -->
         <a class="navbar-brand pt-0" href="./index.html">
-            <img src="./img/brand/blue.png" class="navbar-brand-img" alt="...">
+            <img src="{{ asset('img/logo.png') }}" class="navbar-brand-img" alt="shorter is better">
         </a>
-        <!-- User -->
         <ul class="nav align-items-center d-md-none">
             <li class="nav-item dropdown">
                 <a class="nav-link nav-link-icon" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
