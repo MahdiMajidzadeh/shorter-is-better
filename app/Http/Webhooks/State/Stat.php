551eec77668627b31b4427dd107dda93c7a25be1
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2022-12-08 00:49:12 +0330

    Update Stat.php

--- app/Http/Webhooks/State/Stat.php
+++ app/Http/Webhooks/State/Stat.php
@@ -2,23 +2,81 @@
 
 namespace App\Http\Webhooks\State;
 
+use Illuminate\Support\Facades\DB;
 use App\Http\Webhooks\StateManager;
+use AshAllenDesign\ShortURL\Models\ShortURL;
+use AshAllenDesign\ShortURL\Models\ShortURLVisit;
 
 class Stat extends StateManager
 {
-    public function handle($step, $text = null)
+    public function handleStep1()
     {
-//        if ($step == 1) {
-//
-//        } elseif ($step == 2) {
-//            $this->inputs['url'] = $text;
-//            if($this->isUrl($text)) {
-//                $this->chat->message('send your key')->send();
-//            }
-//        } elseif ($step == 3) {
-//            $this->inputs['key'] = $text;
-//            $this->_makeShort($this->inputs['url'], $this->inputs['key']); // get for send message
-//            $this->lastStep = true;
-//        }
+        $this->chat->message('send shorted url')->send();
+        $this->nextStep();
+    }
+
+    public function handleStep2()
+    {
+        $short = ShortURL::query()->where('default_short_url', $this->message)->first();
+
+        if (!$short) {
+            $this->chat->message('url not fount')->send();
+            return;
+        }
+
+        $this->nonBotData($short->id);
+        $this->visitSummaryData($short->id);
+        $this->done();
+    }
+
+    private function visitSummaryData($id)
+    {
+        $browserStat = ShortURLVisit::where('short_url_id', $id)
+            ->groupBy('browser')
+            ->selectRaw('browser as name, count(*) as num')
+            ->orderBy('num', 'desc')
+            ->get();
+
+        $this->sendStat('Browsers', $browserStat);
+
+        $deviceTypeStat = ShortURLVisit::where('short_url_id', $id)
+            ->groupBy('device_type')
+            ->selectRaw('device_type as name, count(*) as num')
+            ->orderBy('num', 'desc')
+            ->get();
+
+        $this->sendStat('Device Type:', $deviceTypeStat);
+
+        $operatingSystemStat = ShortURLVisit::where('short_url_id', $id)
+            ->groupBy('operating_system')
+            ->selectRaw('operating_system as name, count(*) as num')
+            ->orderBy('num', 'desc')
+            ->get();
+
+        $this->sendStat('Operating System:', $operatingSystemStat);
+    }
+
+    private function nonBotData($id)
+    {
+        $views = ShortURLVisit::query()
+            ->select(DB::raw('Date(visited_at) as name'), DB::raw('count(*) as num'))
+            ->where('visited_at', '>=', now()->subDays(14))
+            ->where('short_url_id', $id)
+            ->whereNot('device_type', 'robot')
+            ->groupBy(DB::raw('Date(visited_at)'))
+            ->get();
+
+        $this->sendStat('Non Bot Data:', $views);
+    }
+
+    private function sendStat($title, $stats)
+    {
+        $text = $title . "\n\n";
+
+        foreach ($stats as $stat) {
+            $text .= $stat->name . ': ' . $stat->num . "\n";
+        }
+
+        $this->chat->message($text)->send();
     }
 }
